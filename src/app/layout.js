f1907a257f94b10b0fdc44716b239584e72b79f8
enamahmedshahaz <enamahmedshahaz@gmail.com>
2024-12-14 12:50:19 +0600

    move header to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header></Header>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}  >
+
+        <div className='flex justify-between bg-red-400 px-5 py-4'>
+          <div>
+            <Link href="/">
+              <h1 className='text-2xl font-semibold text-teal-400'> NextJs <span className='text-indigo-500'>CRUD</span></h1>
+            </Link>
+          </div>
+
+          <div className='flex justify-between gap-4'>
+
+            <Link href="/">Products list</Link>
+
+            <Link href="/create">Create product</Link>
+
+          </div>
+        </div>
+
         <div>
           {children}
         </div>
